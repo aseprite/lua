8dffcf2097b7f893c338acf5db739ed1d36bbc55
David Capello <davidcapello@gmail.com>
2020-04-15 16:26:44 -0300

    Disable os.tmpname()

--- loslib.c
+++ loslib.c
@@ -164,6 +164,7 @@ static int os_rename (lua_State *L) {
 
 
 static int os_tmpname (lua_State *L) {
+#if 0
   char buff[LUA_TMPNAMBUFSIZE];
   int err;
   lua_tmpnam(buff, err);
@@ -171,6 +172,9 @@ static int os_tmpname (lua_State *L) {
     return luaL_error(L, "unable to generate a unique filename");
   lua_pushstring(L, buff);
   return 1;
+#else
+  return 0;
+#endif
 }
 
 
@@ -406,4 +410,3 @@ LUAMOD_API int luaopen_os (lua_State *L) {
   luaL_newlib(L, syslib);
   return 1;
 }
-
